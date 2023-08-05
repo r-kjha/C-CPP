7f98c9a1573b91e2808ea271302de0f3ea2911cf
r-kjha <mail@rkjha.com.np>
2023-08-05 15:45:18 +0545

    new dsa cg

--- CG/lab10/rotation.cpp
+++ CG/lab10/rotation.cpp
@@ -2,23 +2,32 @@
 
 #include <graphics.h>
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     int gd = DETECT, gm;
-    float x1, y1, x2, y2, theta;
+    double x1, y1, x2, y2, r11, r12, r21, r22, xn, yn;
+    float theta;
 
     initgraph(&gd, &gm, (char*)" ");
 
     printf("Enter the coordinate of the line: (x1,y1,x2,y2)\n");
-    scanf("%f,%f,%f,%f", &x1, &y1, &x2, &y2);
+    scanf("%lf,%lf,%lf,%lf", &x1, &y1, &x2, &y2);
 
     line(x1, y1, x2, y2);
 
     printf("Enter the angle of rotation: (theta)\n");
     scanf("%f", &theta);
 
-    r11 = 
+    r11 = cos(theta*3.1428/180);
+    r12 = sin(theta*3.1428/180);
+    r21 = -sin(theta*3.1428/180);
+    r22 = cos(theta*3.1428/180);
     
+    xn=((x2*r11)-(y2*r12));
+    yn=((x2*r12)+(y2*r11));
+
+    line(x1, y1, xn, yn);
 
     getch();
     closegraph();
